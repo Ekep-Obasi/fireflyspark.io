b858db00099ff900c984b1edaa325168548bf116
Ekep Obasi <arreyettaekep@gmail.com>
2025-11-03 15:56:00 -0500

    chore: updated reddit pixel id

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -71,7 +71,7 @@ export function RedditPixel() {
       dangerouslySetInnerHTML={{
         __html: `
           !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);
-          rdt('init','a2_hx8c6oj3em44');
+          rdt('init','a2_gs13uxk5gtd7');
           rdt('track', 'PageVisit');
         `,
       }}
